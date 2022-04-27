73214139bc1cd894ba22ddb45ed2fe749b56ae8d
张东权 <13076539418@163.com>
2022-04-27 23:54:41 +0800

    fix:修复删除画布后弹窗没有关闭的bug

--- src/views/MainPage/MainPage.vue
+++ src/views/MainPage/MainPage.vue
@@ -81,6 +81,7 @@
     private checkIcon(type: string) {
       if (type === 'empty') {
         this.showView(false);
+        Bus.$emit('show-dialog', '', false);
       } else if (type === 'preview') {
         this.$router.push('./preview');
         Bus.$emit('show-dialog', '', false);
