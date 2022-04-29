0d71d98fb2316d6c6c020050df7ff859ae27204e
张东权 <13076539418@163.com>
2022-04-29 13:27:23 +0800

    fix:修复属性重置失败的bug

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -1,7 +1,7 @@
 import { Attrs } from '@/eva/data/Attrs';
 export function toCssStyle(style: string, value: string, unit: string) {
   if (Attrs[style].type === 'input') {
-    return value + unit;
+    return value + (value && unit ? unit : '');
   } else if (Attrs[style].type === 'colorInput') {
     return '#' + value;
   } else {
